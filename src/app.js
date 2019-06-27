5157791411f60435044c75898a4d5c151c94db84
lixiaotao <lixiaotao@hotmail.com>
2019-06-27 18:43:14 +1200

    stetup production wepack build

--- src/app.js
+++ src/app.js
@@ -10,13 +10,30 @@ import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 
 const store = configureStore();
 
 
+store.dispatch(addExpense({ description: 'water bill',amount:4500 }));
+store.dispatch(addExpense({ description: 'gas bill',createdAt:1000  }));
+store.dispatch(addExpense({ description: 'rent',amount:109500 }));
 
+/*
+store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'));
+},3000);
+*/
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log("visible expenses");
+console.log(visibleExpenses);
+console.log("store state");
+console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
