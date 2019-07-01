faa6dcba162e22a42bd6a254bd6a79cc5a241e45
lixiaotao <lixiaotao@hotmail.com>
2019-07-01 15:39:49 +1200

    finish section 13

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilter from './ExpenseListFilter';
+import ExpenseSummary from './ExpensesSummary';
 const ExpenseDashboardPage = () => (
     <div>
-        <h1>test</h1>
+       <ExpenseSummary />
         <ExpenseListFilter />
         <ExpenseList />
     </div>
